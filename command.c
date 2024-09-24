7a3003a5286cee569f5aa770bd6eaf91515a2e00
lindlands <lindlands@outlook.com>
2024-09-24 14:50:00 -0700

    docstrings and better var names

--- command.c
+++ command.c
@@ -5,8 +5,14 @@
 
 #include "macros.h"
 
+#define CHAR_TO_NUM 48
+
+/**
+ * Converts string to lowercase
+ * 
+ * @param str: string
+ */
 void toLower(char* str){
-    /*converts string to lowercase*/
     int i;
     int j = strlen(str);
     for(i = 0; i < j; i++){
@@ -23,18 +29,23 @@ void getCommand(char *arr){
     printf("\n");
 }
 
-int compCom(char *input, char *match){
-    /*compares two strings to see if they match*/
+/**
+ * Compares two strings to see if they match
+ * 
+ * @param inputOne: string
+ * @param inputTwo: string
+ * @return int: 0 if the two inputs match
+ */
+int compCom(char *inputOne, char *inputTwo){
     int result;
     int len;
-    //toLower(match);
-    len = strlen(match);
-    if(strlen(input) < len){
+    len = strlen(inputTwo);
+    if(strlen(inputOne) < len){
         return 0;
     }
-    result = strncmp(input,match,len);
+    result = strncmp(inputOne,inputTwo,len);
     if(result == 0){
-        if ( !result && input[len] && (input[len] != '\n' || input[len+1]) ){
+        if ( !result && inputOne[len] && (inputOne[len] != '\n' || inputOne[len+1]) ){
             return 0;
         }
         return 1;
@@ -42,27 +53,38 @@ int compCom(char *input, char *match){
     return 0;
 }
 
-int convertToNum(char* command){
-    /* takes in string and returns int if possible. else, returns -1. */
+/**
+ * Converts given string to a positive integer.
+ * 
+ * @param str: string
+ * @return int: positive integer or -1 if not possible
+ */
+int convertToNum(char* str){
     int i;
-    for (i = 0; i < strlen(command) - 1; i++){
-        if(!isdigit(command[i])){
+    for (i = 0; i < strlen(str) - 1; i++){
+        if(!isdigit(str[i])){
             return -1;
         }
     }
-    return atoi(command);
+    return atoi(str);
 }
 
+/**
+ * takes in string and fills in array with numbers from string 
+ * 
+ * @param str: string
+ * @param nums: empty array 
+ * @return int: 0 if successful
+ */
 int parseCommand(char *str, int nums[INPUTLEN]){
-    /*takes in string and fills in array with numbers from string*/
     int j = 0;
     int i;
     for (i = 0; i < strlen(str); i++){
         if (isdigit(str[i])){
             if (nums[j] == 0){
-                nums[j] = str[i] - 48;
+                nums[j] = str[i] - CHAR_TO_NUM;
             }else{
-                nums[j] = nums[j] * 10 + (str[i] - 48);
+                nums[j] = nums[j] * 10 + (str[i] - CHAR_TO_NUM);
             }
         }else if(str[i] == ' '){
             j++;
